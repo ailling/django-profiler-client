ce8cc4201eee8ef7a3968b6e6aec48c82ddd7388
Alan Illing <alan.illing@gmail.com>
2012-03-07 20:40:36 -0600

    0.1.2

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 
 from django_profiler_client import version
 
 setup(
     name = "django_profiler_client",
-    package_dir={'django_profiler_client': ''},
     packages = ['django_profiler_client',],
-    include_package_data=False,
     version = version.__VERSION__,
     author = "Alan Illing",
-    description = ("Django Profiler Client is the profiler client for a Django project, available at djangoprofiler.com"),
+    description = ("Django Profiler Client for profiling services at djangoprofiler.com"),
     license = "GPL",
     url = "https://github.com/ailling/django-profiler-client",
-
 )
